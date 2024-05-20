67db1efb09c01f7250d6ef19f55e18b547c56df8
Michael <michael.aron.blue@gmail.com>
2024-05-19 20:07:39 -0600

    here

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const app = express();
 const apiKey = process.env.API_KEY;
 
 // Serve static files from the 'public' directory
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'Public')));
 
 // Endpoint to get the API key
 app.get('/config', (req, res) => {
@@ -16,7 +16,7 @@ app.get('/config', (req, res) => {
 
 // Catch-all route to serve index.html for any other routes
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.sendFile(path.join(__dirname, 'Public', 'index.html'));
 });
 
 const PORT = process.env.PORT || 3000;
